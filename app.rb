aea4f7d8085432128bd944e7a0157962233d339e
Nicholas Rodriguez <niko876@hotmail.com>
2019-06-14 14:34:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,12 +15,12 @@ class App < Sinatra::Base
 
   get "/goodbye/:name" do
     @user_name = params[:name]
-    "Goodbye #{@user_name}."
+    "Goodbye, #{@user_name}."
   end
 
   get "/multiply/:num1/:num2" do
     @product = params[:num1].to_i * params[:num2]
     "#{@product}"
   end
-    
+
 end
